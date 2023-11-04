f199c713d3ff6f6ec7aaa98fe33571aea09fc1d2
Jishnu P V <144010965+Jishnupv23@users.noreply.github.com>
2023-11-04 08:54:39 +0530

    Update urls.py
    Updated

--- hospital_records_project/hospital_records_app/urls.py
+++ hospital_records_project/hospital_records_app/urls.py
@@ -2,16 +2,16 @@ from .views import DepartmentView, DoctorView, DoctorDetailView, PatientView,Pat
 from django.urls import path  
   
 urlpatterns = [  
-    path('departments/', DepartmentView.as_view()),
-    path('doctors/', DoctorView.as_view()),
+    path('departments/', DepartmentView.as_view(),name='departments'),
+    path('doctors/', DoctorView.as_view(),name='doctors'),
     path('doctors/<int:id>', DoctorDetailView.as_view()),
-    path('patients/', PatientView.as_view()),
+    path('patients/', PatientView.as_view(),name='patients'),
     path('patients/<int:id>', PatientDetailView.as_view()),
     path('patient_records/', PatientRecordsView.as_view()),
     path('department/<int:pk>/doctors', DepartmentView.as_view()),
-    # path('login/', UserLoginView.as_view(), name='login'),
-    # path('register/', UserRegistrationView.as_view(), name='register'),
-    # path('logout/', DeleteTokenView.as_view(), name='logout'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('logout/', DeleteTokenView.as_view(), name='logout'),
     
 
 ]  
